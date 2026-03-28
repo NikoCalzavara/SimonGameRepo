f894a6226db83ca164889a1ca9b2cbe597cc23c8
NikoCalzavara <niko.cal.04@gmail.com>
2026-03-28 15:37:03 +0100

    Test iniziale

--- app/src/main/java/com/example/simongame/MainActivity.kt
+++ app/src/main/java/com/example/simongame/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             SimonGameTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Nico",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
