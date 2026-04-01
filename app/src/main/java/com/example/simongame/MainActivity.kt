58d5534d479b1c1f4b19f4160a85c60c285dd34f
NikoCalzavara <niko.cal.04@gmail.com>
2026-04-01 18:50:51 +0200

    Miglioramenti grafici

--- app/src/main/java/com/example/simongame/MainActivity.kt
+++ app/src/main/java/com/example/simongame/MainActivity.kt
@@ -16,8 +16,10 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.ui.graphics.Color
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -52,31 +54,29 @@ class MainActivity : ComponentActivity() {
 fun Schermata1(modifier: Modifier = Modifier) {
     val orientation = LocalConfiguration.current.orientation
     if(orientation == Configuration.ORIENTATION_PORTRAIT) { // Layout verticale
-        Column(modifier = modifier,
+        Column(modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp), // Aggiungo del padding per non avere tutto a filo dello schermo
             verticalArrangement = Arrangement.SpaceEvenly,
             horizontalAlignment = Alignment.CenterHorizontally
         ){
 
-
             // Matrice 3x2
             Matrice(
-                modifier = Modifier.weight(1f),
+                modifier = Modifier.weight(3f),
                 onColorClick = { }
             )
 
-            Spacer(modifier = Modifier.weight(0.1f))
-
             // Testo multi riga non editabile
-            Text(modifier = modifier,
+            Text(modifier = modifier
+                .padding(vertical = 24.dp), // Aggiungo padding solo in verticale, non ai lati
                 text = "Test",
                 fontSize = 30.sp,
                 fontWeight = FontWeight.Bold
             )
 
-            Spacer(modifier = Modifier.weight(0.1f))
-
             // I due bottoni
-            Pulsanti(modifier = Modifier.weight(0.5f))
+            Pulsanti(modifier = Modifier.fillMaxWidth())
 
         }
     }
@@ -96,17 +96,17 @@ fun Schermata1(modifier: Modifier = Modifier) {
 fun Matrice(modifier: Modifier = Modifier, onColorClick: () -> Unit){
     // Per costruire la matrice 3x2 utilizzo una Column con 3 Row al suo interno
     Column(modifier = modifier){
-        Row( modifier = Modifier.weight(0.1f)){
-            Riquadro(Color.Red, Color.White, stringResource(R.string.r), {}, Modifier.weight(1f))
-            Riquadro(Color.Green, Color.White, stringResource(R.string.g), {}, Modifier.weight(1f))
+        Row( modifier = Modifier.weight(1f)){
+            Riquadro(Color.Red, {}, Modifier.weight(1f))
+            Riquadro(Color.Green, {}, Modifier.weight(1f))
         }
-        Row( modifier = Modifier.weight(0.1f)){
-            Riquadro(Color.Blue, Color.White, stringResource(R.string.b), {}, Modifier.weight(1f))
-            Riquadro(Color.Magenta, Color.White, stringResource(R.string.m), {}, Modifier.weight(1f))
+        Row( modifier = Modifier.weight(1f)){
+            Riquadro(Color.Blue, {}, Modifier.weight(1f))
+            Riquadro(Color.Magenta, {}, Modifier.weight(1f))
         }
-        Row( modifier = Modifier.weight(0.1f)){
-            Riquadro(Color.Yellow, Color.Black, stringResource(R.string.y), {}, Modifier.weight(1f))
-            Riquadro(Color.Cyan, Color.White, stringResource(R.string.c), {}, Modifier.weight(1f))
+        Row( modifier = Modifier.weight(1f)){
+            Riquadro(Color.Yellow, {}, Modifier.weight(1f))
+            Riquadro(Color.Cyan,  {}, Modifier.weight(1f))
         }
     }
 }
@@ -116,32 +116,26 @@ fun Riquadro(
     // Funzione compose che costruisce un singolo riquadro secondo i parametri che gli vengono passati
     // Tale funzione viene chiamata sei volte dalla funzione Matrice, per costruire tutti i Box
     coloreRiquadro: Color,
-    coloreLettera: Color,
-    lettera: String,
     onClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
     Box(
         modifier = modifier
             .fillMaxSize()
-            .padding(4.dp)
-            .background(coloreRiquadro)
+            .padding(8.dp) // Padding per spaziare i singoli Box
+            .background(
+                color = coloreRiquadro,
+                shape = RoundedCornerShape(16.dp) // Arrotonda gli angoli dei Box
+            )
             .clickable { onClick() },
         contentAlignment = Alignment.Center // Centra tutto ciò che posiziono nel Box
-    ) {
-        Text( // Lettera dell'iniziale del colore del box
-            text = lettera,
-            fontSize = 35.sp,
-            fontWeight = FontWeight.Bold,
-            color = coloreLettera
-        )
-    }
+    ){}
 }
 
 @Composable
 fun Pulsanti(modifier : Modifier = Modifier) {
     Row(
-        modifier = modifier.fillMaxSize(),
+        modifier = modifier,
         horizontalArrangement = Arrangement.SpaceEvenly
     ) {
         Button(onClick = { /*TODO*/ }) {
