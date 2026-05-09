4b7786cbaf38c8faaa61ed364a5b49989bcf8f04
NikoCalzavara <niko.cal.04@gmail.com>
2026-05-09 10:46:01 +0200

    Inizio implementazione generazione sequenza casuale

--- app/src/main/java/com/example/simongame/GameViewModel.kt
+++ app/src/main/java/com/example/simongame/GameViewModel.kt
@@ -4,7 +4,9 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.simongame.data.PartitaDao
+import kotlinx.coroutines.launch
 
 /* Utilizzo la seguente classe per separare completamente la logica di gioco dall'interfaccia utente.
 *  Così facendo sarò in grado di gestire la logica del gioco in maniera indipendente dalle varie ricomposizioni della UI,
@@ -13,16 +15,26 @@ import com.example.simongame.data.PartitaDao
 class GameViewModel(private val dao: PartitaDao) : ViewModel() {
 
     // All'interno del ViewModel colloco tutte le variabili necessarie al funzionamento del gioco
+    // Utilizzo mutableStateOf in modo da scatenare la ricomposizione della UI dopo i cambiamenti
+
+    private val coloriDisponibili = listOf("R", "G", "B", "M", "Y", "C")
+    var coloreAttivo by mutableStateOf<String?>(null) // Colore che sta "suonando"
+        private set // Imposto tutte le variabili private set così posso solamente leggerle dalla UI ma non modificarle. Il metodo get è pubblico, il metodo set è privato
 
     var sequenzaComputer by mutableStateOf(listOf<String>())
+        private set
 
     var sequenzaGiocatore by mutableStateOf(listOf<String>())
+        private set
 
     var isTurnoComputer by mutableStateOf(false)
+        private set
 
     var partitaInCorso by mutableStateOf(false)
+        private set
 
     var isInPausa by mutableStateOf(false)
+        private set
 
     fun avviaPartita() { // Invocata quando si preme "Avvia partita"
         partitaInCorso = true
@@ -30,6 +42,32 @@ class GameViewModel(private val dao: PartitaDao) : ViewModel() {
         sequenzaComputer = emptyList()
         sequenzaGiocatore = emptyList()
 
+        sequenzaComputer = listOf(coloriDisponibili.random()) // Primo colore casuale
+
+        riproduciSequenzaComputer()
+    }
+
+    private fun riproduciSequenzaComputer(){ // Riproduzione di un colore alla volta
+        viewModelScope.launch { // "Launch" fa partire l'operazione in background
+            for (colore in sequenzaComputer){
+
+                while (isInPausa){
+                    kotlinx.coroutines.delay(100) // Ogni 100 millisecondi controlla lo stato della variabile isInPausa. Se è True NON procede
+                }
+
+                coloreAttivo = colore // "Accendo" il colore
+
+                kotlinx.coroutines.delay(500) // Tengo acceso il colore per mezzo secondo
+
+                coloreAttivo = null // "Spengo" il colore
+
+                kotlinx.coroutines.delay(250) // Pausa prima di procedere con il prossimo colore
+
+            }
+
+            isTurnoComputer = false // Finito di suonare la sequenza tocca al giocatore
+
+        }
     }
 
     fun pausaRiprendi() {
@@ -37,7 +75,6 @@ class GameViewModel(private val dao: PartitaDao) : ViewModel() {
     }
 
     fun colorePremuto(coloreCliccato: String) { // Chiamata quando l'utente clicca un riquadro
-
     }
 
     fun finePartita() {
